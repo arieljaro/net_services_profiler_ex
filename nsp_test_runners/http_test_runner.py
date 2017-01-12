c928f96fae4901203ed3d2b8ac613e80c43555e6
arieljaro <arieljaro@gmail.com>
2017-01-12 15:42:12 +0200

    small change in log output of HTTPConnection

--- nsp_test_runners/http_test_runner.py
+++ nsp_test_runners/http_test_runner.py
@@ -18,6 +18,7 @@ class HTTPTestRunner(AbstractTestRunner):
     def _initialize(self):
         self._response = None
         self._failure_reason = None
+        self._connection_type = None
 
     def _run_test(self):
 
@@ -25,6 +26,7 @@ class HTTPTestRunner(AbstractTestRunner):
         parse_result = urlparse(self.target_name)
 
         try:
+            self._connection_type = 'HTTPSConnection' if parse_result.scheme == 'https' else 'HTTPConnection'
             http_connection_type = http.client.HTTPSConnection if parse_result.scheme == 'https' \
                 else http.client.HTTPConnection
             conn = http_connection_type(parse_result.netloc)
@@ -51,10 +53,11 @@ class HTTPTestRunner(AbstractTestRunner):
 
     def get_test_descriptive_result(self):
         if self.test_result:
-            return 'Successfully connected to {} - (Response status: {:d})'.format(self.target_name, self._http_status)
+            return 'Successful {} to {} - (Response status: {:d})'.format(
+                self._connection_type, self.target_name, self._http_status)
         else:
-            return 'Failed to connect to {} - Failure reason: {} (Response status: {})'.format(
-                self.target_name, self._failure_reason.value, self._http_status)
+            return 'Failed {} to connect to {} - Failure reason: {} (Response status: {})'.format(
+                self._connection_type, self.target_name, self._failure_reason.value, self._http_status)
 
     @property
     def _http_status(self):
