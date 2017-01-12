c8c577d22bd2e671d630168e8744344581e2860d
arieljaro <arieljaro@gmail.com>
2017-01-12 16:04:54 +0200

    implemented HTTPConnection stats (bandwidth + latency)

--- nsp_test_runners/http_test_runner.py
+++ nsp_test_runners/http_test_runner.py
@@ -3,9 +3,12 @@ from enum import Enum
 from urllib.parse import urlparse
 import socket
 
+import time
+
 from nsp_test_runners.abstract_test_runner import AbstractTestRunner
 
 HTTP_STATUS_ERROR_OFFSET = 400
+
 
 class HTTPFailureReason(Enum):
     RESOLUTION_FAILED = 'dns resoultion failed'
@@ -19,6 +22,7 @@ class HTTPTestRunner(AbstractTestRunner):
         self._response = None
         self._failure_reason = None
         self._connection_type = None
+        self._latency = None
 
     def _run_test(self):
 
@@ -30,18 +34,25 @@ class HTTPTestRunner(AbstractTestRunner):
             http_connection_type = http.client.HTTPSConnection if parse_result.scheme == 'https' \
                 else http.client.HTTPConnection
             conn = http_connection_type(parse_result.netloc)
+
+            self._latency = -time.time()
             conn.request("GET", parse_result.path)
+            self._latency += time.time()
+
             self._response = conn.getresponse()
 
+            # calculate bandwidth
             if self._http_status > HTTP_STATUS_ERROR_OFFSET:
                 self._failure_reason = HTTPFailureReason.BAD_HTTP_STATUS
             else:
                 test_result = True
+                contents = self._response.read()
+                self._bandwidth = (len(contents) / self._latency) / 1000.0
 
         except socket.gaierror:
             self._failure_reason = HTTPFailureReason.RESOLUTION_FAILED
         except:
-            self._failure_reason = HTTPFailureReason.OTHER_REASON
+            self._failure_reason = HTTPFailureReason.UNEXPECTED_ERROR
 
         self.test_result = test_result
 
@@ -49,12 +60,16 @@ class HTTPTestRunner(AbstractTestRunner):
         return True
 
     def get_test_stats(self):
-        return dict()
+        return {
+            'bandwidth': self._bandwidth,
+            'latency': self._latency
+        }
 
     def get_test_descriptive_result(self):
         if self.test_result:
-            return 'Successful {} to {} - (Response status: {:d})'.format(
-                self._connection_type, self.target_name, self._http_status)
+            return 'Successful {} to {} - (Response status: {:d}) ' \
+                   '[Download bandwidth of {:.2f} KBps, latency of {:.2f} seconds]'.format(
+                self._connection_type, self.target_name, self._http_status, self._bandwidth, self._latency)
         else:
             return 'Failed {} to connect to {} - Failure reason: {} (Response status: {})'.format(
                 self._connection_type, self.target_name, self._failure_reason.value, self._http_status)
